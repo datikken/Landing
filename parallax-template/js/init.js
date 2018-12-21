5a251ab6d38eaea4c0e2742852c696c13e833dbb
Tikken <tikken23@gmail.com>
2018-12-21 15:16:19 +0200

    animated text

--- parallax-template/js/init.js
+++ parallax-template/js/init.js
@@ -6,3 +6,11 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+jQuery(document).ready(function(){
+  $('h1').mousemove(function(e){
+    var rXP = (e.pageX - this.offsetLeft-$(this).width()/2);
+    var rYP = (e.pageY - this.offsetTop-$(this).height()/2);
+    $('h1').css('text-shadow', +rYP/10+'px '+rXP/80+'px rgba(121,121,128), '+rYP/8+'px '+rXP/60+'px rgba(238,238,238), '+rXP/70+'px '+rYP/12+'px rgba(0,0,0)');
+  });
+});
